119e410a90d7035fbccbec1ee0bd6ebb46c705c7
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-07-07 15:41:30 +0200

    com2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Classe User, no html
+
 class User {
   constructor(_firstName, _lastName, _age, _location) {
     this.firstName = _firstName;
@@ -26,6 +28,8 @@ let s = gino.confront(pippo);
 
 console.log(s);
 
+//Lista Animali
+
 class Pets {
   constructor(_nome, _padrone, _specie, _razza) {
     this.nome = _nome;
@@ -33,26 +37,43 @@ class Pets {
     this.specie = _specie;
     this.razza = _razza;
   }
+
+  sameOwner = function (otherPet) {
+    if (this.padrone === otherPet.padrone) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
 
 let n = 0;
-
+let nome = document.getElementById("nomeInput");
+let padrone = document.getElementById("padroneInput");
+let specie = document.getElementById("specieInput");
+let razza = document.getElementById("razzaInput");
+let table = document.querySelector("tbody");
+//uso un array per avere istanze diverse
+let petsArray = [];
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let nome = document.getElementById("nomeInput");
-  let padrone = document.getElementById("padroneInput");
-  let specie = document.getElementById("specieInput");
-  let razza = document.getElementById("razzaInput");
-  let table = document.querySelector("tbody");
+
   n++;
   const pet = new Pets(nome.value, padrone.value, specie.value, razza.value);
-  console.log(pet);
+  petsArray.push(pet);
   table.innerHTML += `<tr>
                   <th scope="row">${n.toString()}</th>
-                  <td>${pet.nome}</td>
-                  <td>${pet.padrone}</td>
-                  <td>${pet.specie}</td>
-                  <td>${pet.razza}</td>
+                  <td>${petsArray[n - 1].nome}</td>
+                  <td>${petsArray[n - 1].padrone}</td>
+                  <td>${petsArray[n - 1].specie}</td>
+                  <td>${petsArray[n - 1].razza}</td>
                 </tr>`;
+
+  console.log(petsArray);
 });
+
+const lady = new Pets("lady", "giuseppe", "gatto", "bo");
+const tyson = new Pets("tyson", "pasquale", "cane", "labrador");
+
+console.log(tyson.sameOwner(lady));
